c74631ca1d77af0b33a737fdba4386155f999a7b
eberarielteodorohernandez-droid <eberarielteodorohernandez@gmail.com>
2026-02-15 11:56:06 -0600

    prueba

---
+++ Persona.cpp
@@ -0,0 +1,25 @@
+# include <iostream>
+using namespace std; 
+
+
+class persona{
+//Atributos
+protected :
+	string nombres,apellidos,direccion,fecha_nacimiento;
+	int telefono;
+//Constructores
+	persona(){
+	}
+		persona(string nom, string ape, string dir, int tel, string fn){
+			nombres = nom;
+			apellidos= ape;
+			direccion= dir;
+			telefono = tel;
+			fecha_nacimiento = fn;	
+		}
+//Metodo CRUD	
+	void crear();
+	void leer();
+	void actualizar();
+	void borrar();
+};
